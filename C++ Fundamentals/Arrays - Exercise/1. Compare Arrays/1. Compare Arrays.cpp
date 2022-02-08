64fc89e108fcdf4be1f9da055bf52e2d70e7797c
Martin Pavlov <65560174+Marto86@users.noreply.github.com>
2022-02-08 18:17:35 +0200

    Reffactoring

--- C++ Fundamentals/Arrays - Exercise/1. Compare Arrays/1. Compare Arrays.cpp
+++ C++ Fundamentals/Arrays - Exercise/1. Compare Arrays/1. Compare Arrays.cpp
@@ -1,53 +1,61 @@
 #include <iostream>
 #include <array>
 
-const int maxSize = 200;
+const int maxSize = 1000;
+
+bool areEqual(const std::array<int, maxSize>& arr1, int length1,
+	const std::array<int, maxSize>& arr2, int length2)
+{
+	if (length1 != length2)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < length1; ++i)
+	{
+		if (arr1[i] != arr2[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+const std::array<int, maxSize> readInput(int& actualSize)
+{
+	std::array<int, maxSize>arr;
+
+	std::cin >> actualSize;
+	for (int i = 0; i < actualSize; ++i)
+	{
+		std::cin >> arr[i];
+	}
+
+	return arr;
+}
+
+
+
 
 int main()
 {
+	int arrOneSize = 0;
+	const std::array<int, maxSize> arrOne = readInput(arrOneSize);
+	int arrTwoSize = 0;
+	const std::array<int, maxSize> arrTwo = readInput(arrTwoSize);
 
-    const std::array<int, maxSize> readInput(int& actualArrSize);
-    bool areEqual(const std::array<int, maxSize> &arr1, int length1, const std::array<int, maxSize> &arr2, int length2);
-
-    int arr1Size = 0;
-    const std::array<int, maxSize> arr1 = readInput(arr1Size);
-    int arr2Size = 0;
-    const std::array<int, maxSize> arr2 = readInput(arr2Size);
-
-    const bool result = areEqual(arr1, arr1Size, arr2, arr2Size);
-	 if (result)
-    {
-        std::cout << "equal" << std::endl;
-    }
-    else
-    {
-        std::cout << "not equal" << std::endl;
-    }
-    return 0;
+	const bool result = areEqual(arrOne, arrOneSize, arrTwo, arrTwoSize);
+	if (result)
+	{
+		std::cout << "Equal" << std::endl;
+	}
+	else
+	{
+		std::cout << "Not Equal" << std::endl;
+	}
+	return 0;
 }
 
-const std::array<int, maxSize> readInput(int& actualArrSize)
-{
-    std::array<int, maxSize> arr{};
-    std::cin >> actualArrSize;
-    for (int i = 0; i < actualArrSize; ++i)
-    {
-        std::cin >> arr[i];
-    }
-    return arr;
-}
-bool areEqual(const std::array<int, maxSize>& arr1, int length1, const std::array<int, maxSize>& arr2, int length2)
-{
-    if (length1 != length2)
-    {
-        return false;
-    }
-    for (int i = 0; i < arr1.size(); ++i)
-    {
-        if (arr1[i] != arr2[i])
-        {
-            return false;
-        }
-    }
-    return true;
-}
+
+
+
