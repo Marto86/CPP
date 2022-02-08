a3f464bbb9133c633d3b1f3643d860a05db92ed7
Martin Pavlov <65560174+Marto86@users.noreply.github.com>
2022-02-08 21:44:26 +0200

    Write function for calculation

--- C++ Fundamentals/Arrays - Exercise/2. Longest Sequence/2. Longest Sequence.cpp
+++ C++ Fundamentals/Arrays - Exercise/2. Longest Sequence/2. Longest Sequence.cpp
@@ -27,30 +27,49 @@ std::array<int, maxSize > readInput(int& actualSize)
 
 
 
-void findLongestSequence(const std::array< int, maxSize>& arr ,int arrSize)
+void findLongestSequence(const std::array< int, maxSize>& arr ,
+    int arrSize, 
+    int& frequentNumber,
+    int& numberRepsCount)
 {
-    int frequentNumber = 0;
-    int numberRepsCount = 0;    
+    frequentNumber = arr[0];
+    numberRepsCount = 1;
+    int currentRespCount = 1;
+	for (int i = 1; i < arrSize - 1; ++i)
+	{
+		if (arr[1] == arr[i + 1])
+		{
+            currentRespCount++;
+            continue;
+		}
+		if (currentRespCount >= numberRepsCount)
+		{
+            numberRepsCount = currentRespCount;
+            frequentNumber = arr[i];
+		}
+        currentRespCount = 1;
+	}
+
+
 }
-void printOutput(int& maxSequence, int& element)
+void printOutput(int& frequntNumber, int& numberRespCount)
 {
-    for (int i = 0; i <= maxSequence; ++i)
+
+    for (int i = 0; i <= numberRespCount; ++i)
     {
-        if (i == maxSequence)
-        {
-            std::cout << element;
-        }
-        else
-        {
-            std::cout << element << " ";
-        }
+        std::cout << frequntNumber << ' ';
     }
+    std::cout << std::endl;
 }
 
 int main()
 {
+    int frequentNumber = 0;
+    int numberRepsCount = 0;
     int arrSize = 0;
     const std::array< int, maxSize> arr = readInput(arrSize);
+    findLongestSequence(arr,arrSize,frequentNumber,numberRepsCount);
+    printOutput(frequentNumber, numberRepsCount);
 
     return 0;
 }
